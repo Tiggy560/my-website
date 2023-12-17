0a2b6016a1b3c3f20fa0b6d08f4aca6525055685
Tiggy560 <will@paretolabs.xyz>
2023-12-17 14:57:35 -0800

    updates to format

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <main>
+        <div className="content">
         <div className="header">
           <h1>Will McTighe</h1>
           <ul>
@@ -22,10 +23,10 @@ function App() {
             <h2>Web3 stuff:</h2>
             <ul>
               <li><a href="https://www.web3analytic.xyz/">Web3 Analytic</a> - a marketing platform for Web3 projects</li>
-              <li>How to Build a DeFi Order Book [<a href="https://github.com/pareto-xyz/pareto-order-book-whitepaper/blob/main/how_to_orderbook.pdf">whitepaper</a>] [<a href="https://github.com/pareto-xyz/pareto-core-v1">smart contract code</a>] [<a href="https://github.com/pareto-xyz/pareto-orderbook-v1">orderbook code</a>] [<a href="https://github.com/pareto-xyz/pareto-app-v1">frontend code</a>] [<a href="https://twitter.com/WillMcTighe/status/1626292936076931079">tweet</a>]</li>
-              <li>DeFi options vault using RMMs [<a href="https://github.com/pareto-xyz/pareto-theta-vault-whitepaper/blob/main/whitepaper.pdf">whitepaper</a>] [<a href="https://github.com/pareto-xyz/pareto-theta-vault-v1">code</a>] [<a href="https://twitter.com/WillMcTighe/status/1599812098820694017">tweet</a>]</li>
-              <li>New family of AMM invariants [<a href="https://arxiv.org/abs/2205.07452">whitepaper</a>] [<a href="https://twitter.com/mike_h_wu/status/1526601203773083653">tweet</a>] [<a href="https://www.smartcontractresearch.org/t/research-summary-constant-power-root-market-makers/1698">scrf</a>]</li>
-              <li><a href="https://tutela.xyz/">Tutela</a>, a tool to cluster blockchain wallets owned by the same entity [<a href="https://arxiv.org/abs/2201.06811">whitepaper</a>] [<a href="https://github.com/pareto-xyz/tutela-app">code</a>] [<a href="https://twitter.com/WillMcTighe/status/1475564099568381960">tweet</a>] [<a href="https://golem.foundation/2022/02/07/interview-Tutela.html">press</a>]</li>
+              <li>How to Build a DeFi Order Book [<a href="https://github.com/pareto-xyz/pareto-order-book-whitepaper/blob/main/how_to_orderbook.pdf">whitepaper</a>] </li>
+              <li>DeFi options vault using RMMs [<a href="https://github.com/pareto-xyz/pareto-theta-vault-whitepaper/blob/main/whitepaper.pdf">whitepaper</a>] </li>
+              <li>New family of AMM invariants [<a href="https://arxiv.org/abs/2205.07452">whitepaper</a>] [<a href="https://www.smartcontractresearch.org/t/research-summary-constant-power-root-market-makers/1698">scrf</a>]</li>
+              <li><a href="https://tutela.xyz/">Tutela</a>, a blockchain anonymity assessment tool [<a href="https://arxiv.org/abs/2201.06811">whitepaper</a>] [<a href="https://golem.foundation/2022/02/07/interview-Tutela.html">press</a>]</li>
             </ul>
           </div>
           <div className="section">
@@ -36,6 +37,7 @@ function App() {
               <li>Investment Banking at <a href="https://www.goldmansachs.com/">Goldman Sachs</a></li>
               <li>Mathematical Economics and some Philosophy at University of Warwick (UK)</li>
             </ul>
+          </div>
           </div>
         </div>
       </main>
