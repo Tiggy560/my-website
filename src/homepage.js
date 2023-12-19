7a2394f9d394e0f0ce5511efd7469bbc72976fa4
Tiggy560 <will@paretolabs.xyz>
2023-12-19 14:28:05 -0800

    adding startup journey article

---
+++ src/homepage.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './homepage.css';
+
+function HomePage() {
+  return (
+    <div className="App">
+      <main>
+        <div className="content">
+        <div className="header">
+          <h1>Will McTighe</h1>
+          <ul>
+            <div>
+              Building <a href="https://www.web3analytic.xyz/">Web3 Analytic</a> and documenting it <Link to="/startup-journey">here</Link>.
+            </div>
+            <div>
+              Contact: <a href="https://twitter.com/WillMcTighe">Twitter</a>, will[at]paretolabs.xyz
+            </div>
+          </ul>
+          <hr />
+          <div className="section">
+            <h2>Crypto:</h2>
+            <ul>
+              <li>Now building <a href="https://www.web3analytic.xyz/">Web3 Analytic</a> - a marketing platform for Web3 apps</li>
+              <li>
+                  Co-authored several DeFi papers on:
+                  <ul>
+                    <li>How to build an Order Book in DeFi [<a href="https://github.com/pareto-xyz/pareto-order-book-whitepaper/blob/main/how_to_orderbook.pdf">whitepaper</a>]</li>
+                    <li>How to use a special type of AMM to build a DeFi Options Vault [<a href="https://github.com/pareto-xyz/pareto-theta-vault-whitepaper/blob/main/whitepaper.pdf">whitepaper</a>]</li>
+                    <li>A new family of AMM curves [<a href="https://arxiv.org/abs/2205.07452">whitepaper</a>]</li>
+                  </ul>
+                </li>
+              <li>With a couple of friends at Stanford, we built <a href="https://tutela.xyz/">Tutela</a>, a blockchain anonymity assessment tool [<a href="https://arxiv.org/abs/2201.06811">whitepaper</a>] [<a href="https://golem.foundation/2022/02/07/interview-Tutela.html">press</a>]</li>
+            </ul>
+          </div>
+          <div className="section">
+            <h2>My journey getting here:</h2>
+            <ul>
+              <li>Born in the UK, I grew up moving all over as a young kid - 11 houses across 3 continents by the age of 7.</li>
+              <li>I studied Mathematical Economics and Philosophy and when I graduated, I had a lot of energy and wanted to be challenged but not that much direction so gave Finance a try.</li>
+              <li>After 5 years working in Banking (<a href="https://www.goldmansachs.com/">Goldman Sachs</a>) and tech investing (<a href="https://www.vitruvianpartners.com/">Vitruvian Partners</a>), I knew that I wanted to come to the US and build companies.</li>
+              <li>I got into Stanford to do a MBA and co-founded <a href="https://www.web3analytic.xyz/">Web3 Analytic</a>, when I finished in mid-2022.</li>
+              <li>More on company building journey later!</li>
+            </ul>
+          </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
---
+++ src/startupjourney.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './startupjourney.css';
+
+function StartupJourney() {
+  return (
+    <div className="App">
+      <main>
+        <div className="content">
+        <div className="header">
+            <h1>My Startup Journey</h1>
+            
+            <ul> Startup journeys often get rewritten after the fact and young founders idolize outcomes without appreciating the pain of the journey, so I’m documenting my steps and pivots here. </ul>
+
+            <hr/>
+
+            <h3>Idea 1: A DeFi Options Vault (Pareto Vaults) </h3>
+                <ul>
+                    <li>In June 2022, my co-founder and I got overly very excited by a new technology in DeFi.</li>
+                    <li>We built an options vault that theoretically should have produced higher returns than existing vaults but didn’t test that assumption before building. When we eventually did market research after 2 months of building, we realized, it wasn’t solving a user need.</li>
+                </ul>    
+                <h3>Learnings:</h3>
+                    <ul>
+                        <li>Talk to users, identify their problems, test your assumptions</li>
+                        <li>A Solution in Search of a Problem is very dangerous</li>
+                    </ul>
+                <hr/>
+            <h3>Idea 2: DeFi Options Order Book (Pareto Order Book)</h3>
+                <ul>
+                    <li>In our market research, we learnt that capital efficiency was one of the reasons on-chain options hadn’t worked so far and so decided to build an options order book, like DYDX-v3, which addressed this.</li>
+                    <li>After another 2 months of building, we came to the conclusion that this didn’t play to our strengths - neither of us had been options traders before and my co-founder had a PhD in Machine Learning that we weren't using.</li>
+                </ul>
+                <h3>Learnings:</h3>
+                    <ul>
+                        <li>Founder-market fit matters</li>
+                        <li>Figma (lol)</li>
+                    </ul>
+                <hr/>
+            <h3>Idea 3: Marketing and Analytics Tool for Web3 Teams (Web3 Analytic)</h3>
+                <ul>
+                    <li>Talking with other DeFi founders, we learnt that they struggled to grow their user bases and contacting potential customers was very hard.</li>
+                    <li>This was a very tangible problem and since launch in April, we’ve built a customer base of ~20 protocols and grown to ~$90k ARR. Let’s see how the rest of this journey goes.</li>
+                </ul>
+                <h3>Learnings (so far):</h3>
+                    <ul>
+                        <li>Build faster MVPs - don’t spend 6 months building before launching if possible</li>
+                        <li>Market potential and how you’ll capture it REALLY matters.</li>
+                        <li>Choose your business model wisely. Crypto SaaS is hard - there are not many Web3 companies and when market activity grows 10x, the number of potential customers does not, so if you are selling a SaaS product and want to build a venture scale business, the contract sizes need to be large (e.g. $100k+), everyone needs to use your product (i.e. Horizontal SaaS) or sell to Web2 entities (e.g. Chainalysis, TRM Labs). Volume-based business models (e.g. CEXs, DEXs, Lending Protocols, RPCs) have more upside.</li>
+                        <li>When navigating the idea maze, make decisions that solve user needs AND connect with your vision</li>
+                    </ul>
+        </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default StartupJourney;
